c6edfb31110142a126117303ed65a6586043cd39
Omar Gonzalez <gomarzalez@gmail.com>
2023-11-03 22:07:52 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,72 @@
+
+
+// getToken function
+const getToken = async () => {
+    const response = await fetch ('https://accounts.spotify.com/api/token',{
+        method: 'POST',
+        body: 'grant_type=client_credentials',
+        headers: {
+            'Authorization': `Basic ${btoa(clientId + ':' + clientSecret)}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    const data = await response.json()
+    const token = data.access_token
+    return token
+}
+
+// verify that we get our token
+// getToken()
+
+// use getToken function, to get a song
+// getSong Function
+const getSong = async (track, artist) => {
+    const token = await getToken()
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${track}&type=track`,{ 
+        headers:{
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    const data = await response.json()
+    const songsArr = data.tracks.items
+    for(let song of songsArr) {
+        if(song.artists[0].name === artist) {
+            return song.preview_url
+        }
+    }
+}
+
+// verify that we get the song data
+
+//  cashing the dom for each song div
+const allTracks = document.querySelectorAll('.title-text')
+const allArtists = document.querySelectorAll('.artist-text')
+
+// clickedSong function
+const clickedSong = async (divId) => {
+    const track = allTracks[divId.slice(-1)].innerText
+    const artist = allArtists[divId.slice(-1)].innerText
+    const songUrl = await getSong(track, artist)
+    return playSong(songUrl)
+
+}
+
+// handles playing the audio
+let audio;
+const playSong = (url) => {
+    if (audio){
+        audio.pause()
+        audio = ''
+    }
+    audio = new Audio(url)
+    audio.play()
+}
+
+
+// handles pausing the audio
+const stopBtn = document.querySelector('#stopBtn')
+stopBtn.addEventListener('click', () => {
+    if (audio){
+    audio.pause()
+    }
+})
